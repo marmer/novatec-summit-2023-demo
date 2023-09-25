60531223fb839d1ab4e8d787f3fa8bbf83eeffc9
Mariano Mertinat <mariano.mertinat@novatec-gmbh.de>
2023-09-25 17:44:42 +0200

    Initial commit

---
+++ src/test/java/io/marmer/github/demo/novatecsummit2023demo/NovatecSummit2023DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.marmer.github.demo.novatecsummit2023demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovatecSummit2023DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
